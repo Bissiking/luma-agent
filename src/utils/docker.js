e8b8a6258b1e504e5b8cfc387b48acffbc1a5b4e
Mathéo HEMERY <matheo@mhemery.fr>
2024-12-07 21:51:53 +0100

    Update docker.js

--- src/utils/docker.js
+++ src/utils/docker.js
@@ -4,18 +4,26 @@ const { exec } = require('child_process');
 // Vérifie si le fichier docker.sock existe
 function isDockerAvailable() {
     const dockerSockPath = '/var/run/docker.sock';
-    const dockerSockVar = fs.existsSync(dockerSockPath);
-    console.log(dockerSockPath);
-    
-    return dockerSockVar
+    fs.stat(dockerSockPath, function (err, stat) {
+        if (err == null) {
+            return true
+        } else if (err.code === 'ENOENT') {
+            // file does not exist
+            return false
+        } else {
+            console.log('Some other error: ', err.code);
+            return false
+        }
+    });
+
 }
 
 // Vérifie les conteneurs Docker si docker.sock est présent
 function checkDockerContainers() {
     return new Promise((resolve, reject) => {
         if (!isDockerAvailable()) {
-            console.log('DOCKER OFF -> '+isDockerAvailable());
-            
+            console.log('DOCKER OFF -> ' + isDockerAvailable());
+
             resolve(false); // Si Docker n'est pas disponible
             return;
         }
